224a7081e5c463d82315ae4a0e961ec79de1ad6d
ophir <devteam@wizer.me>
2018-12-10 15:11:50 +0200

    fix to the correct download function

--- downloader.js
+++ downloader.js
@@ -9,41 +9,8 @@ const DIR_NAME = 'requests';
 if (!fs.existsSync(DIR_NAME)){
     fs.mkdirSync(DIR_NAME);
 }
+
  function download(song){
-     console.log(`title: ${song.title}`);
-     if(!song.title){
-         song.title =`TITLE-ME ${song.id}`;
-     }
-     if(!song.id){
-         song.id = 'oVbXpK_BRbw' // bohemian raphsody
-         song.title = 'ERASE-ME!';
-         }
-         let fileName = `${DIR_NAME}/${sanitize(song.title)}.mp4`;
-        if(!fs.existsSync(fileName)){
-            const videoUrl = `https://www.youtube.com/watch?v=${song.id}`;
-            let download = ytdl(videoUrl,{quality: 'highest', filter: (format) => format.container === 'mp4'})
-            .pipe(fs.createWriteStream(fileName));
-
-            download.on('error', function(err) {
-                console.log(`got error on file ${fileName}`);
-                console.error(err);
-                });    
-            
-            download.on('finish', function(){
-                console.log(`downloaded file ${fileName}`);
-                const stats = fs.statSync(fileName);
-                const fileSizeInBytes = stats.size;
-                console.log(`got ${fileSizeInBytes} bytes.`);
-            });
-
-        }else{
-            console.log(`skipping file ${fileName} - exists allready!`);
-        }
-
-
- }
-
- function downloadP(song){
      return new Promise((resolve, reject)=>{
 
         if(!song.title){
